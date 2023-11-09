2b72d9019994553db1674f27d52baa26635af057
tomascamargo123 <54809925+tomascamargo123@users.noreply.github.com>
2023-11-09 17:54:27 -0300

    Add files via upload

---
+++ Parcial html/Parcial html/casas.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', TraerDatos);
+function TraerDatos(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'casas.json', true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let datos = JSON.parse(this.responseText);
+            let info =document.querySelector('#info')
+            info.innerHTML = "";
+            for(let item of datos){
+               //console.log(item)
+               info.innerHTML += `
+               <div class="contenedor-principal">
+               <div class="precio-info">
+               <img src="${item.img}" width="300px" alt="imagen2" class="img-articulo"> 
+               
+               <p>
+                   ${item.info}
+               </p>
+           </div>
+           </div>    
+               `
+            };
+            info.appendChild(div);
+        }
+
+    }
+}
+
